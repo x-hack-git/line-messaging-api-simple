9842240e61cd48570c2bb198c078a68305379d5e
Your Name <youremail@example.com>
2021-01-11 23:11:52 +0900

    update with shell command

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var request = require('request');
+var bodyParser = require("body-parser");
+var request = require("request");
 var crypto = require("crypto");
-var async = require('async');
+var async = require("async");
 
-var sendMessage = require('./lib/sendMessage.js');
-var messageTemplate = require('./lib/messageTemplate.js');
+var sendMessage = require("./lib/sendMessage.js");
+var messageTemplate = require("./lib/messageTemplate.js");
 
 // utilモジュールを使います。
-var util = require('util');
+var util = require("util");
 
-app.set('port', (process.env.PORT || 8000));
+app.set("port", process.env.PORT || 8000);
 // JSONの送信を許可
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 // JSONパーサー
 app.use(bodyParser.json());
 
 // herokuのTOPページに表示されるコンテンツを指定する
-app.get('/', function(req, res) {
-  res.send('<h1>hello world</h1>');
+app.get("/", function (req, res) {
+  res.send("<h1>hello world</h1>");
 });
 
 // async.waterfall([function(){}], function(){})
-app.post('/callback', function(req, res) {
+app.post("/callback", function (req, res) {
   async.waterfall(
     [
-      function(callback) {
+      function (callback) {
         // リクエストがLINE Platformから送られてきたか確認する
-        if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+        if (!validate_signature(req.headers["x-line-signature"], req.body)) {
           return;
         }
 
@@ -40,46 +42,54 @@ app.post('/callback', function(req, res) {
         }
 
         // LINE Platformから送られてきたデータからユーザIDを取得する
-        var eventData = req.body['events'][0];
-        var user_id = eventData['source']['userId'];
-        var message_id = eventData['message']['id'];
+        var eventData = req.body["events"][0];
+        var user_id = eventData["source"]["userId"];
+        var message_id = eventData["message"]["id"];
         // 'text', 'image' ...
-        var message_type = eventData['message']['type'];
-        var message_text = eventData['message']['text'];
+        var message_type = eventData["message"]["type"];
+        var message_text = eventData["message"]["text"];
 
         // ユーザー以外から届いたデータは無視する
-        if (eventData['source']['type'] !== 'user') return 
+        if (eventData["source"]["type"] !== "user") return;
 
         // ユーザー情報取得
-        request.get(getProfileOption(user_id), function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            // 次のメソッドを呼ぶ
-            callback(req, body['displayName'], message_id, message_type, message_text);
+        request.get(
+          getProfileOption(user_id),
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              // 次のメソッドを呼ぶ
+              callback(
+                req,
+                body["displayName"],
+                message_id,
+                message_type,
+                message_text
+              );
+            }
           }
-        });
+        );
       },
     ],
 
-    function(req, displayName, message_id, message_type, message_text) {
-      const replyMessages = []
+    function (req, displayName, message_id, message_type, message_text) {
+      const replyMessages = [];
       var message = "hello, " + displayName + "さん"; // helloと返事する
-      
-      //var message = message_text; // おうむ返しする
-      //var message = message_text + "[" + message_text.length + "文字]";
-      replyMessages.push(messageTemplate.textMessage(message))
-      
+
+      // var message = message_text; // おうむ返しする
+      // var message = message_text + "[" + message_text.length + "文字]";
+      // replyMessages.push(messageTemplate.textMessage(message));
+
       ///////////////////
       // 画像で返事をする //
       ///////////////////
-      
-      // var messages = ["左上を押した", "右上を押した", "左下を押した", "右下を押した"];
-      // if (message_text == "猫") {
-      //    replyMessages.push(messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/8cbL5dl.jpg'));
-      // } else if (message_text == "犬") {
-      //    replyMessages.push(messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/ph82KWH.jpg'));
-      // } else if (message_text == "鹿") {
-      //    replyMessages.push(messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/Z6ilhSI.jpg'));
-      // }
+
+      if (message_text == "猫") {
+        replyMessages.push(messageTemplate.imageMessage("https://i.imgur.com/8cbL5dl.jpg"));
+      } else if (message_text == "犬") {
+        replyMessages.push(messageTemplate.imageMessage("https://i.imgur.com/ph82KWH.jpg"));
+      } else if (message_text == "鹿") {
+        replyMessages.push(messageTemplate.imageMessage("https://i.imgur.com/Z6ilhSI.jpg"));
+      }
 
       sendMessage.send(req, replyMessages);
       return;
@@ -87,20 +97,20 @@ app.post('/callback', function(req, res) {
   );
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running');
+app.listen(app.get("port"), function () {
+  console.log("Node app is running");
 });
 
 // 送られてきたメッセージタイプをbotの内容に合わせる
 // 一致すればtrueを返す
 function isValidDataType(req) {
-  var eventType = req.body['events'][0];
-  var messageType = eventType['message']['type'];
+  var eventType = req.body["events"][0];
+  var messageType = eventType["message"]["type"];
 
-  if (eventType['type'] != 'message') {
+  if (eventType["type"] != "message") {
     return false; // メッセージではない
   }
-  if (messageType != 'text' && messageType != 'image') {
+  if (messageType != "text" && messageType != "image") {
     return false; // テキストでも画像でもない
   }
   return true; // valid
@@ -108,16 +118,22 @@ function isValidDataType(req) {
 
 function getProfileOption(user_id) {
   return {
-    url: 'https://api.line.me/v2/bot/profile/' + user_id,
+    url: "https://api.line.me/v2/bot/profile/" + user_id,
     proxy: process.env.FIXIE_URL,
     json: true,
     headers: {
-      'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
-    }
+      Authorization: "Bearer {" + process.env.LINE_CHANNEL_ACCESS_TOKEN + "}",
+    },
   };
 }
 
 // 署名検証
 function validate_signature(signature, body) {
-  return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+  return (
+    signature ==
+    crypto
+      .createHmac("sha256", process.env.LINE_CHANNEL_SECRET)
+      .update(new Buffer(JSON.stringify(body), "utf8"))
+      .digest("base64")
+  );
 }
--- heroku_create.sh
+++ heroku_create.sh
@@ -1,3 +1,2 @@
-heroku create 
-heroku config:set LINE_CHANNEL_SECRET=<your line channel secret>
-heroku config:set LINE_CHANNEL_ACCESS_TOKEN=<your line access token>
+heroku config:set LINE_CHANNEL_SECRET=9b94449a60b16a815c39c422c40bcd27
+heroku config:set LINE_CHANNEL_ACCESS_TOKEN=2DyNBoMuJpFL5vpgYhTDeP0a4WtgXBmnMTQnijAaspBqjTHWqp5DMVdlzhjDBwQH3wuuhOsHlicUFWCYIEH/YTseUuPG6K4o55DNLtKMzTzUK9OBWP+v8cW4oYNgvjDadoBE1sS+MN4hbmY10kDszwdB04t89/1O/w1cDnyilFU=
