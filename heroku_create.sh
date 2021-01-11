37d6d1141bd7c25049ee56d49c3b24162f01de92
Your Name <youremail@example.com>
2021-01-11 23:11:52 +0900

    update with shell command

--- heroku_create.sh
+++ heroku_create.sh
@@ -1,3 +1,2 @@
-heroku create 
-heroku config:set LINE_CHANNEL_SECRET=<your line channel secret>
-heroku config:set LINE_CHANNEL_ACCESS_TOKEN=<your line access token>
+heroku config:set LINE_CHANNEL_SECRET=*****
+heroku config:set LINE_CHANNEL_ACCESS_TOKEN=*****
